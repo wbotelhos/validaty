662d74a8bd1bafe257706772069e5fd9fa3a51cd
Washington Botelho <wbotelhos@gmail.com>
2020-05-04 12:27:00 -0300

    up: karma file

--- karma.conf.js
+++ karma.conf.js
@@ -2,26 +2,20 @@ module.exports = function(config) {
   'use strict';
 
   config.set({
-    browsers:   ['Chrome', 'Firefox'],
-    debug:      true,
-    frameworks: ['jasmine', 'fixture'],
-    singleRun:  true,
+    browsers: ['Chrome', 'Firefox'],
+    debug:    true,
 
     files: [
       'node_modules/jquery/dist/jquery.min.js',
-      'spec/fixtures/*.html',
-
-
       'lib/*.css',
       'lib/*.js',
-
+      'spec/fixtures/*.html',
       'spec/spec_helper.js',
-
       'spec/javascripts/**/*.js'
     ],
 
-    preprocessors: {
-      '**/*.html': ['html2js']
-    }
+    frameworks:    ['jasmine', 'fixture'],
+    preprocessors: { '**/*.html': ['html2js'] },
+    singleRun:     true,
   });
 };
