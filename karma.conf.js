2759132d9f777f6f478b6bba1be27f71a9b1255b
Washington Botelho <wbotelhos@gmail.com>
2023-08-20 20:56:25 -0300

    rm: firefox launch

--- karma.conf.js
+++ karma.conf.js
@@ -2,20 +2,20 @@ module.exports = function(config) {
   'use strict';
 
   config.set({
-    browsers: ['Chrome', 'Firefox'],
-    debug:    true,
+    browsers: ["Chrome"],
+    debug: true,
 
     files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'lib/*.css',
-      'lib/*.js',
-      'spec/fixtures/*.html',
-      'spec/spec_helper.js',
-      'spec/javascripts/**/*.js'
+      "node_modules/jquery/dist/jquery.min.js",
+      "lib/*.css",
+      "lib/*.js",
+      "spec/fixtures/*.html",
+      "spec/spec_helper.js",
+      "spec/javascripts/**/*.js",
     ],
 
-    frameworks:    ['jasmine', 'fixture'],
-    preprocessors: { '**/*.html': ['html2js'] },
-    singleRun:     true,
+    frameworks: ["jasmine", "fixture"],
+    preprocessors: { "**/*.html": ["html2js"] },
+    singleRun: true,
   });
 };
